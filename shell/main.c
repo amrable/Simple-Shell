b3b273264b732b31987ab84814eebb5e2ca6ace1
amrfahmy <amrfahmy57@gmail.com>
2019-10-06 13:21:27 +0200

    Initial Commit

---
+++ shell/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  shell
+//
+//  Created by Amr Fahmy on 10/6/19.
+//  Copyright © 2019 Amr Fahmy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
